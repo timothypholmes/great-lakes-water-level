a5efa70bc0108d413137dfb9ec3e8fed0fbb669a
Timothyholmes11 <tpholmes7@gmail.com>
2019-06-07 10:09:39 -0500

    Update

--- lake_michigan_depth.py
+++ lake_michigan_depth.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd 
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
+from os import path
 #import seaborn as sns
 
 
@@ -37,14 +38,16 @@ ax24 = fig.add_subplot(224, projection='3d')
 
 x = date[0]
 y = date[1]
-c = lake_michigan_huron_depth
+c_mich = lake_michigan_huron_depth
+c_super = lake_Superior_depth
+c_erie = lake_Erie_depth
+c_onta = lake_Ontario_depth
 
-img = ax21.scatter(x, y, lake_michigan_huron_depth, c=c, cmap=plt.cm.cividis)
-img = ax22.scatter(x, y, lake_Superior_depth, c=c, cmap=plt.cm.cividis)
-img = ax23.scatter(x, y, lake_Erie_depth, c=c, cmap=plt.cm.cividis)
-img = ax24.scatter(x, y, lake_Ontario_depth, c=c, cmap=plt.cm.cividis)
-cbar_ax = fig.add_axes([0.5, 0, 0.03, 0.1])
-fig.colorbar(img, orientation = "horizontal")
+img = ax21.scatter(x, y, lake_michigan_huron_depth, c=c_mich, cmap=plt.cm.cividis)
+img1 = ax22.scatter(x, y, lake_Superior_depth, c=c_super, cmap=plt.cm.cividis)
+img2 = ax23.scatter(x, y, lake_Erie_depth, c=c_erie, cmap=plt.cm.cividis)
+img3 = ax24.scatter(x, y, lake_Ontario_depth, c=c_onta, cmap=plt.cm.cividis)
+
 #winter
 #coolwarm
 #ocean
@@ -53,33 +56,41 @@ ax21.title.set_text('Lake Michigan-Huron')
 
 ax21.set_xlabel('Month')
 ax21.set_ylabel('Year')
-ax21.set_zlabel('Depth (Meters)')
-ax21.view_init(azim=0, elev=0)
+ax21.set_zlabel('Mean Water Level (Meters)')
+ax21.view_init(azim=90, elev=0)
 #ax22.view_init(azim=90, elev=0)
+ax21.w_xaxis.line.set_lw(0.)
+ax21.set_xticks([])
 
 ax22.title.set_text('Lake Superior')
 
 ax22.set_xlabel('Month')
 ax22.set_ylabel('Year')
-ax22.set_zlabel('Depth (Meters)')
-ax22.view_init(azim=0, elev=0)
+ax22.set_zlabel('Mean Water Level (Meters)')
+ax22.view_init(azim=90, elev=0)
 #ax22.view_init(azim=90, elev=0)
+ax22.w_xaxis.line.set_lw(0.)
+ax22.set_xticks([])
 
 ax23.title.set_text('Lake Erie')
 
 ax23.set_xlabel('Month')
 ax23.set_ylabel('Year')
-ax23.set_zlabel('Depth (Meters)')
-ax23.view_init(azim=0, elev=0)
+ax23.set_zlabel('Mean Water Level (Meters)')
+ax23.view_init(azim=90, elev=0)
 #ax22.view_init(azim=90, elev=0)
+ax23.w_xaxis.line.set_lw(0.)
+ax23.set_xticks([])
 
 ax24.title.set_text('Lake Ontario')
 
 ax24.set_xlabel('Month')
 ax24.set_ylabel('Year')
-ax24.set_zlabel('Depth (Meters)')
-ax24.view_init(azim=0, elev=0)
+ax24.set_zlabel('Mean Water Level (Meters)')
+ax24.view_init(azim=90, elev=0)
 #ax22.view_init(azim=90, elev=0)
+ax24.w_xaxis.line.set_lw(0.)
+ax24.set_xticks([])
 
 
 '''
@@ -98,8 +109,9 @@ x = range(0, len(x))
 #Jan 2010 to present (May 2019)
 yg = ax.plot(x[1105:1217], lake_michigan_huron_depth[1105:1217])
 
-plt.savefig('lakes2.png')
-plt.savefig('lakes2.pdf')
+outpath = "~/Documents/github/great-lakes-water-level/"
+
+fig.savefig(path.join(outpath,"lakes.pdf"))
 plt.show()
 
 
